1b3552eab048f3173e68cb6f03c338a44db61147
zzdx713 <zzdx713@126.com>
2025-10-17 14:23:07 +0800

    Update __manifest__.py

--- wechat_verification/__manifest__.py
+++ wechat_verification/__manifest__.py
@@ -1,15 +1,32 @@
+# -*- coding: utf-8 -*-
 {
-    'name': '企业微信验证模块',
-    'version': '1.0',
-    'author': 'Admin',
-    'category': 'Tools',
-    # 关键：依赖web模块（Odoo 18控制器需依赖）
-    'depends': ['base', 'web'],
-    'data': [],
-    # 关键：允许安装+自动加载
+    'name': "Odoo-钉钉",
+    'summary': """该应用是为了解决企业微信域名验证而开发在odoo18验证通过""",
+    'description': """将该目录复制到addon目录登陆odoo安装企业微信验证应用，通过http://ip:端口/WW_verify_bKh5CGfbqD7k5qGy.txt""",
+    'author': "XueFeng.Su",
+    'website': "https://github.com/cd-feng",
+    'category': 'Dingtalk/HR',
+    'version': '18.0.0.1',
+    'depends': ['hr'],
+    "license": "AGPL-3",
     'installable': True,
-    'auto_install': True,
-    # 标记为应用，避免状态异常
-    'application': False,
-    'summary': '企业微信可信域名验证文件服务'
+    'application': True,
+    'auto_install': False,
+    'data': [
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+        'data/default_ir_cron.xml',
+
+        'views/menu.xml',
+        'views/dingtalk_setting.xml',
+        'views/hr_department.xml',
+        'views/hr_employee.xml',
+        'views/res_users.xml',
+
+        'wizard/hr_department_wizard.xml',
+        'wizard/hr_employee_wizard.xml',
+    ],
+    'images': [
+        'static/description/image_1.png',
+    ],
 }
